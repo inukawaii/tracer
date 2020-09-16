bdd9de8f2e357ed1b4bf42b80a0f68221bd597ff
mayumi aka inukawaii <inukawaii999@gmail.com>
2020-09-16 16:00:48 +0900

    clickイベント時のfunctionを追加

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -3,13 +3,25 @@ import '@blueprintjs/core/lib/css/blueprint.css';
 
 import { Button, FormGroup, InputGroup, Intent } from "@blueprintjs/core";
 
+interface Props {}
+
 export default class ProfileForm extends Component {
   intent: Intent = Intent.PRIMARY;
+
+  constructor(props: Props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // TODO
+  }
+
   render() {
     return (
       <FormGroup>
         <InputGroup id="text-input" placeholder="なまえ" />
-        <Button>つくる</Button>
+        <Button onClick={this.handleClick}>つくる</Button>
       </FormGroup>
     );
   }
